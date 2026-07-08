b7b08f3baa8dd88554255ce1378e8570e7add248
Ranveer <ranveersangwan22@gmail.com>
2026-07-08 11:04:11 +0530

    Auto-run migrations during Vercel build

--- build_files.sh
+++ build_files.sh
@@ -4,3 +4,6 @@ python3 -m pip install -r requirements.txt
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
+
+echo "Running migrations..."
+python3 manage.py migrate --noinput
