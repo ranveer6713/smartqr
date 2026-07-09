fba82706932e3b1c0e93b49cbed91c1c09b18347
Ranveer <ranveersangwan22@gmail.com>
2026-07-09 11:11:57 +0530

    Add --break-system-packages flag to bypass PEP 668 restriction on Vercel build environment

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing project dependencies..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
